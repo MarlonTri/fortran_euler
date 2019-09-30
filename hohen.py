e4ce5315c101f53ff0012a502c0f156c91aa3cdd
MarlonTri <47997412+MarlonTri@users.noreply.github.com>
2019-09-29 17:00:56 -0700

    Add files via upload

--- hohen.py
+++ hohen.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib import animation
 import math
 """size = (50,50)
 spread = 0.05
@@ -13,40 +15,48 @@ plt.imshow(start)
 plt.colorbar()
 plt.show()"""
 
-def swifthohen(epsilon, g):
-    stop=0;
+fig = plt.figure()
 
-    L=40*math.pi;
-    N=512;
-    a0=(4*epsilon/3)**.5;
-    noise = 0.2;
+stop=0;
+epsilon = 0.11
 
-    t=0;
-    dt=0.3/epsilon;
-    U=2*noise*(np.random.random((N,N))-.5)
-    #Ut=[t*np.log(np.absolute(U))*np.log(a0)]
-    q=np.zeros((N,1));
-    for k in range(N):
-      q[k]=(2*(k-1)/N-1)*math.pi*N/L;
 
-    Q=U
-    for k in range(N):
-      for l in range(N):
+L=40*math.pi;
+N=128;
+
+noise = 0.62;
+
+t=0;
+dt=0.1/epsilon;
+U=2*noise*(np.random.random((N,N))-.5)
+im = plt.imshow(U)
+plt.colorbar()
+q=np.zeros((N,1))
+for k in range(N):
+    q[k]=(2*(k-1)/N-1)*math.pi*N/L;
+
+Q=U
+for k in range(N):
+    for l in range(N):
         Q[k,l]=1/(1-dt*(epsilon-(1-(q[k]**2+q[l]**2))**2));
+        
+def swifthohen(i):
+    skip=1
+    
+    for k in range(skip):
+        U = im.get_array()
+        u=np.fft.fftshift(np.fft.fft2(U));
 
-    n=0
-    while t<100/epsilon and stop==0:
-      n=n+1;
-      u=np.fft.fftshift(np.fft.fft2(U));
-      t=t+dt;
-      u=u*Q;
-      U=np.real(np.fft.ifft2(np.fft.ifftshift(u)));
-      U=U+dt*g(U);
-      #Ut.append(t*np.log(np.absolute(U))*np.log(a0))
+        u=u*Q;
+        U=np.real(np.fft.ifft2(np.fft.ifftshift(u)));
+        U=U+dt*((1-0.01*i)*U**2-U**3)
+        im.set_array(U)
+        print(i)
+        
+    return im
 
-      if n%30==0:
-        plt.imshow(U)
-        plt.colorbar()
-        plt.show()
-g = lambda x: -0.02*x**2 - x**3
-swifthohen(.01,g)
+anim = animation.FuncAnimation(fig, swifthohen,
+                               frames=10000, interval=1)
+
+plt.show()
+
